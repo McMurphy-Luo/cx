f08e8541f3e3b31d2bb20099f9291717213a4156
McMurphy Luo <McMurphy.Luo@gmail.com>
2020-07-21 22:40:13 +0800

    Add book recommendation. Add several test cases

--- Functional.cpp
+++ Functional.cpp
@@ -6,16 +6,37 @@ namespace
   class TestClass {
   public:
     TestClass(int value)
-      : value_(value) {
+      : value_(std::make_unique<int>(value)) {
 
     }
 
-    int Calculate(int argument) {
-      return value_ + argument;
+    TestClass(const TestClass& another)
+      : value_(std::make_unique<int>(*another.value_)) {
+
+    }
+
+    TestClass& operator=(const TestClass& another) {
+      if (this == &another) {
+        return *this;
+      }
+      value_ = std::make_unique<int>(*another.value_);
+      return *this;
+    }
+
+    TestClass(TestClass&& another) = default;
+
+    TestClass& operator=(TestClass&& another) = default;
+
+    int Calculate(int argument) const {
+      return *value_ + argument;
+    }
+
+    bool IsValid() const {
+      return !!value_;
     }
 
   private:
-    int value_;
+    std::unique_ptr<int> value_;
   };
 }
 
@@ -24,10 +45,30 @@ TEST_CASE("Test Bind and PlaceHolders") {
   std::function<int(int)> calculator = std::bind(&TestClass::Calculate, &test_object_1, std::placeholders::_1);
   CHECK(calculator(1) == 6);
   CHECK(calculator(13) == 18);
+  calculator = std::bind(std::mem_fn(&TestClass::Calculate), &test_object_1, std::placeholders::_1);
+  CHECK(calculator(99) == 104);
 }
 
+// Prefer lambda to std::bind whenever
+// Effective Modern C++ Rule 34
 TEST_CASE("Test Lambda Expression") {
   TestClass test_object_1(5);
-
-
+  std::function<int(int)> calculator = [&factor = test_object_1](int argument) -> int {
+    return factor.Calculate(argument);
+  };
+  test_object_1 = TestClass{ 9 };
+  CHECK(calculator(12) == 21);
+  CHECK(calculator(80) == 89);
+  calculator = [factor = test_object_1](int argument) -> int {
+    return factor.Calculate(argument);
+  };
+  test_object_1 = 30;
+  CHECK(calculator(9) == 18);
+  CHECK(calculator(11) == 20);
+  // Use init capture to move object into closures
+  // Effective Modern C++ rule 32
+  calculator = [factor = std::move(test_object_1)](int argument) -> int {
+    return factor.Calculate(argument);
+  };
+  CHECK(!test_object_1.IsValid());
 }
--- Variant.cpp
+++ Variant.cpp
@@ -27,12 +27,11 @@ namespace
       > {};
 
   template<typename T>
-  int ReleaseImpl(void* buffer, int index, size_t i) {
+  void ReleaseImpl(void* buffer, int index, size_t i) {
     if (index == i) {
       T* buffer_typed = reinterpret_cast<T*>(buffer);
       delete buffer_typed;
     }
-    return 0;
   }
 
   template<typename... T, size_t... I>
@@ -44,6 +43,42 @@ namespace
   void Release(void* buffer, int index) {
     assert(!buffer && index == -1 || buffer && index >= 0 && index <= sizeof...(T));
     ReleaseUnpack<T...>(buffer, index, std::index_sequence_for<T...>{});
+  }
+
+  template<typename T>
+  void* CopyConstructImpl(void* source) {
+    return new T(*reinterpret_cast<T*>(source));
+  }
+
+  template<typename... T, size_t... I>
+  void* CopyConstructUnpack(int index, void* source, std::index_sequence<I...>) {
+    void* result = nullptr;
+    ((I == index ? (result = CopyConstructImpl<std::tuple_element_t<I, std::tuple<T...>>>(source)) : nullptr), ...);
+    return result;
+  }
+
+  template<typename... T>
+  void* CopyConstruct(int index, void* source) {
+    void* result = CopyConstructUnpack<T...>(index, source, std::index_sequence_for<T...>{});
+    assert(result);
+    return result;
+  }
+
+  template<typename T>
+  int AssignImpl(void* source, void* dest) {
+    *reinterpret_cast<T*>(dest) = *reinterpret_cast<T*>(source);
+    return 0;
+  }
+
+  template<typename... T, size_t... I>
+  void AssignUnpack(int index, void* source, void* dest, std::index_sequence<I...>) {
+    // Optimization does not apply.
+    std::initializer_list<int>{ (I == index ? (AssignImpl<std::tuple_element_t<I, std::tuple<T...>>>(source, dest)) : 0)...};
+  }
+
+  template<typename... T>
+  void Assign(int index, void* source, void* dest) {
+    AssignUnpack<T...>(index, source, dest, std::index_sequence_for<T...>{});
   }
 
   template<typename... T>
@@ -58,7 +93,32 @@ namespace
     Variant(const Variant& another)
       : type_index_(another.type_index_)
       , buffer_(nullptr) {
+      if (another.buffer_) {
+        buffer_ = CopyConstruct<T...>(another.type_index_, another.buffer_);
+      }
+    }
 
+    Variant& operator=(const Variant& another) {
+      if (this == &another) {
+        return *this;
+      }
+      if (type_index_ == another.type_index_) {
+        if (type_index_ == -1) {
+          assert(buffer_ == nullptr);
+          return *this;
+        }
+        Assign<T...>(type_index_, another.buffer_, buffer_);
+        return *this;
+      }
+      if (type_index_ != -1) {
+        Release<T...>(buffer_, type_index_);
+        buffer_ = nullptr;
+      }
+      if (another.type_index_ != -1) {
+        buffer_ = CopyConstruct<T...>(another.type_index_, another.buffer_);
+      }
+      type_index_ = another.type_index_;
+      return *this;
     }
 
     ~Variant() {
@@ -69,36 +129,22 @@ namespace
 
     template<typename U>
     Variant(const U& value)
-      : buffer_(nullptr) {
-      int constexpr type_index = TypeIndex<U, 0, T...>::value;
-      static_assert(type_index != -1);
-      typedef typename std::tuple_element_t<type_index, std::tuple<TypeHelper<T>...>>::type CurrentType;
-      buffer_ = new CurrentType(value);
-      type_index_ = type_index;
-    }
-
-    template<typename U>
-    Variant(U&& value)
-      : buffer_(nullptr) {
-      int constexpr type_index = TypeIndex<U, 0, T...>::value;
-      static_assert(type_index != -1);
-      typedef typename std::tuple_element_t<type_index, std::tuple<TypeHelper<T>...>>::type CurrentType;
-      buffer_ = new CurrentType(value);
-      type_index_ = type_index;
+      : type_index_(TypeIndex<U, 0, T...>::value)
+      , buffer_(nullptr) {
+      static_assert(TypeIndex<U, 0, T...>::value != -1);
+      buffer_ = new U(value);
     }
 
     template<typename U>
     Variant& operator=(const U& another) {
+      static_assert(TypeIndex<U, 0, T...>::value != -1);
       constexpr int new_index = TypeIndex<U, 0, T...>::value;
       int old_index = type_index_;
-      static_assert(new_index != -1);
       if (old_index == new_index) {
         typedef typename std::tuple_element_t<new_index, std::tuple<TypeHelper<T>...>>::type CurrentType;
         CurrentType* current_buffer = reinterpret_cast<CurrentType*>(buffer_);
         (*current_buffer) = another;
-        return *this;
-      }
-      if (old_index != -1) {
+      } else if (old_index != -1) {
         Release<T...>(buffer_, old_index);
         buffer_ = nullptr;
       }
@@ -107,7 +153,7 @@ namespace
       return *this;
     }
 
-    constexpr int index() const {
+    int index() const {
       return type_index_;
     }
 
@@ -154,11 +200,19 @@ TEST_CASE("Test Variant") {
   TestVariant<int, std::string, std::vector<std::string>> test_variant_1(std::string{ "TestTest222" });
   CHECK(test_variant_1.index() == 1);
   CHECK(Get<1>(test_variant_1) == "TestTest222");
-  test_variant_1 = std::vector<std::string>{ "1", "Meeting", "Chat", "Phone" };
+  test_variant_1 = std::vector<std::string>{ "1", "T2T2T2", "Three33333Three", "ZZZABCD" };
   CHECK(test_variant_1.index() == 2);
   TestVariant<int, std::string, std::vector<std::string>> test_variant_2 = test_variant_1;
   CHECK(test_variant_2.index() == 2);
   CHECK(Get<2>(test_variant_2)[0] == "1");
-  CHECK(Get<2>(test_variant_2)[1] == "Meeting");
-  CHECK(Get<2>(test_variant_2)[3] == "Phone");
+  CHECK(Get<2>(test_variant_2)[1] == "T2T2T2");
+  CHECK(Get<2>(test_variant_2)[3] == "ZZZABCD");
+  {
+    TestVariant<int, std::string, std::vector<std::string>> test_variant_3{ std::string{"3Test3"} };
+    test_variant_2 = test_variant_3;
+  }
+  CHECK(test_variant_2.index() == 1);
+  CHECK(Get<1>(test_variant_2) == "3Test3");
+  // CHECK(Get<test_variant_2.index()>());
+  // CHECK(std::get<std::vector<std::string>>(test_variant_2)[2] == "Meeting");
 }
